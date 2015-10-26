99d48639ec15745698a9e37ad63dea715b053754
Stephen Roberts <stephenianroberts@gmail.com>
2015-10-26 13:56:06 +0000

    added HDF5 lite example

--- c/hdf5/lite/src/main.c
+++ c/hdf5/lite/src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "hdf5.h"
+#include "hdf5_hl.h"
 
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 
@@ -14,10 +15,10 @@ const int data_rows = 11;
 const int data_cols = 15;
 
 void write(void) {
-  double output_data[data_rows][data_cols];
+  double *output_data = malloc(data_rows * data_cols * sizeof(double));
   for (int i = 0; i < data_rows; ++i) {
     for (int j = 0; j < data_cols; ++j) {
-      output_data[i][j] = MIN(MIN(i, j)+1, MIN(data_rows-i, data_cols-j));
+      output_data[i * data_cols + j] = MIN(MIN(i, j)+1, MIN(data_rows-i, data_cols-j));
     }
   }
 
@@ -25,42 +26,33 @@ void write(void) {
   printf("Output Deck:\n");
   for (int i = 0; i < data_rows; ++i) {
     for (int j = 0; j < data_cols; ++j) {
-      printf("%.0f,", output_data[i][j]);
+      printf("%.0f,", output_data[i * data_cols + j]);
     }
     printf("\n");
   }
 
-  // N.B. Proper error handling omitted for brevity
-  herr_t status;
-  hid_t file_id, dataset_id, dataspace_id;
+  hid_t file_id;
   hsize_t dims[RANK] = {data_rows, data_cols};
   file_id = H5Fcreate(file_name, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-  dataspace_id = H5Screate_simple(RANK, dims, NULL);
-  /* Create the dataset. */
-  dataset_id = H5Dcreate(file_id, dataset_name, H5T_NATIVE_DOUBLE, dataspace_id, 
-                                 H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-  // Write the data to the dataset
-  status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, output_data);
-  status = H5Dclose(dataset_id);
-  status = H5Fclose(file_id);
+  H5LTmake_dataset(file_id, dataset_name, RANK, dims, H5T_NATIVE_DOUBLE, output_data);
+  H5Fclose(file_id);
+  free(output_data);
 }
 
 void read(void) {
-  double input_data[data_rows][data_cols];
+  double *input_data = malloc(data_rows * data_cols * sizeof(double));
   herr_t status;
   hid_t file_id, dataset_id, dataspace_id;
   file_id = H5Fopen(file_name, H5F_ACC_RDWR, H5P_DEFAULT);
-  dataset_id = H5Dopen(file_id, dataset_name, H5P_DEFAULT);
-  status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, input_data);
-    printf("Input Deck:\n");
+  H5LTread_dataset_double(file_id, dataset_name, input_data);
+  printf("Input Deck:\n");
   for (int i = 0; i < data_rows; ++i) {
     for (int j = 0; j < data_cols; ++j) {
-      printf("%.0f,", input_data[i][j]);
+      printf("%.0f,", input_data[i * data_cols + j]);
     }
     printf("\n");
   }
-
-  status = H5Dclose(dataset_id);
+  free(input_data);
   status = H5Fclose(file_id);
 }
   
