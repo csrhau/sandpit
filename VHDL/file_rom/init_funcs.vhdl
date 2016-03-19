71ff2f631462d4c3f2d116ac7d25ee0591053757
Stephen Roberts <stephenianroberts@gmail.com>
2016-03-19 13:26:01 +0000

    got rid of dead code

--- VHDL/file_rom/init_funcs.vhdl
+++ VHDL/file_rom/init_funcs.vhdl
@@ -11,25 +11,6 @@ end package init_funcs;
 package body init_funcs is
 
   function read_file(data_file_name: string) return memory_16b is
-    constant test_storage : memory_16b := (
-      0  => "00000000",
-      1  => "00000001",
-      2  => "00000010",
-      3  => "00000011",
-      4  => "00000100",
-      5  => "11110000",
-      6  => "11110000",
-      7  => "11110000",
-      8  => "11110000",
-      9  => "11110000",
-      10 => "11110000",
-      11 => "11110000",
-      12 => "11110000",
-      13 => "11110000",
-      14 => "11110000",
-      15 => "11110000"
-    );
-
     variable state : memory_16b;
     variable mem_temp : bit_vector(7 downto 0);
     file data_file : text open read_mode is data_file_name;
