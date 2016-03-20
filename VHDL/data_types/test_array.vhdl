fa73405b672c0a1936de1ef8b6387520b4d9bedc
Stephen Roberts <stephenianroberts@gmail.com>
2016-03-20 16:45:23 +0000

    elaborated example

--- VHDL/data_types/test_array.vhdl
+++ VHDL/data_types/test_array.vhdl
@@ -6,8 +6,11 @@ end test_array;
 
 architecture behavioural of test_array is
   -- This is how to specify unconstrained ranges.
-  type board is array(integer range <>, integer range <>) of integer range 0 to 1;
-  signal state : board(5 downto 0, 5 downto 0);
+  type board1 is array(integer range <>, integer range <>) of integer range 0 to 1;
+  type board2 is array(5 downto 0, 5 downto 0) of integer range 0 to 1;
+  -- Can't seem to define board2 in terms of board1 :(.
+  signal state1 : board1(5 downto 0, 5 downto 0);
+  signal state2 : board2;
 begin
   process
   begin
