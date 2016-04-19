ad1a28dc969a2ce704af79f8a54acbd259b25084
Stephen Roberts <stephenianroberts@gmail.com>
2016-04-19 18:13:08 +0100

    shown some simple clock logic

--- VHDL/testbenches/testbench.vhdl
+++ VHDL/testbenches/testbench.vhdl
@@ -1,21 +1,25 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-
-
 entity testbench is 
 end entity testbench;
 
 
 architecture behavioural of testbench is
+  constant period : time := 10 ns;
   signal active : std_logic := '1';
   signal clock : std_logic := '0';
 
 begin
-  clock <= not clock after 5 ns when active = '1';
-
+  clock <= not clock after period / 2 when active = '1';
   process
   begin
+
+    wait for period;
+    wait for period;
+    wait for period;
+    wait for period;
+
     wait for 100 ns;
     active <= '0';
     wait;
