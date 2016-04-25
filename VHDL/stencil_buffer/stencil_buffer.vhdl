ddffdaf0ab7b511e4a04ac6ab7eb7847b94750f8
Stephen Roberts <stephenianroberts@gmail.com>
2016-04-25 15:09:42 +0100

    added kick ass stencil buffer code

--- VHDL/stencil_buffer/stencil_buffer.vhdl
+++ VHDL/stencil_buffer/stencil_buffer.vhdl
@@ -2,31 +2,75 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- NB in practice the stencil buffer shouldn't expose all these values;
+-- More commonly, the stencil buffer will be a stencil engine, and produce
+-- A single output based on some f(tl, tc, tr... br)
+-- This file exists to help understand and iron out offsets
+
 entity stencil_buffer is
   port (
     clock : in std_logic;
     advance: in std_logic;
     address: in std_logic_vector;
     input : in std_logic_vector;
-    output : out std_logic_vector
+    tl : out std_logic_vector;
+    tc : out std_logic_vector;
+    tr : out std_logic_vector;
+    ml : out std_logic_vector;
+    mc : out std_logic_vector;
+    mr : out std_logic_vector;
+    bl : out std_logic_vector;
+    bc : out std_logic_vector;
+    br : out std_logic_vector
   );
 end entity stencil_buffer;
 
 architecture behavioural of stencil_buffer is
+  type direction_t is (NORTH_WEST, NORTH,  NORTH_EAST,
+                       WEST,       CENTER,       EAST,
+                       SOUTH_WEST, SOUTH,  SOUTH_EAST);
+
+  function offset_addr(address: std_logic_vector; direction: direction_t) return natural is
+    variable offset : natural range 1 to 9;
+  begin
+    case direction is
+      when NORTH_WEST => offset := 9;
+      when NORTH      => offset := 8;
+      when NORTH_EAST => offset := 7;
+      when WEST       => offset := 6;
+      when CENTER     => offset := 5;
+      when EAST       => offset := 4;
+      when SOUTH_WEST => offset := 3;
+      when SOUTH      => offset := 2;
+      when SOUTH_EAST => offset := 1;
+    end case;
+    return to_integer(unsigned(address) - offset);
+  end function offset_addr;
+
   type neighbourhood_t is array(integer range 0 to (2**address'length-1)) of std_logic_vector(input'range);
   signal neighbourhood : neighbourhood_t := (others => (others => '0'));
   signal head : std_logic_vector(address'range) := (others => '0');
-begin
 
-  process(clock)
-  begin
+begin
+  SEQUENTIAL: process(clock) begin
     if rising_edge(clock) then
       if advance = '1' then
         neighbourhood(to_integer(unsigned(head))) <= input;
         head <= std_logic_vector(unsigned(head) + 1);
       end if;
-      output <= neighbourhood(to_integer(unsigned(address)));
     end if;
+  end process;
+
+  COMBI: process (head) begin
+    tl <= neighbourhood(offset_addr(head, NORTH_WEST));
+    tc <= neighbourhood(offset_addr(head, NORTH));
+    tr <= neighbourhood(offset_addr(head, NORTH_EAST));
+    ml <= neighbourhood(offset_addr(head, WEST));
+    mc <= neighbourhood(offset_addr(head, CENTER));
+    mr <= neighbourhood(offset_addr(head, EAST));
+    bl <= neighbourhood(offset_addr(head, SOUTH_WEST));
+    bc <= neighbourhood(offset_addr(head, SOUTH));
+    br <= neighbourhood(offset_addr(head, SOUTH_EAST));
   end process;
 
 end behavioural;
--- VHDL/stencil_buffer/test_stencil_buffer.vhdl
+++ VHDL/stencil_buffer/test_stencil_buffer.vhdl
@@ -13,51 +13,99 @@ architecture behavioural of test_stencil_buffer is
       advance: in std_logic;
       address: in std_logic_vector;
       input : in std_logic_vector;
-      output : out std_logic_vector
+      tl : out std_logic_vector;
+      tc : out std_logic_vector;
+      tr : out std_logic_vector;
+      ml : out std_logic_vector;
+      mc : out std_logic_vector;
+      mr : out std_logic_vector;
+      bl : out std_logic_vector;
+      bc : out std_logic_vector;
+      br : out std_logic_vector
     );
   end component stencil_buffer;
 
+  signal period: time := 10 ns;
   signal clock : std_logic := '0';
   signal finished : std_logic := '0';
+
   signal advance : std_logic := '0';
+  signal address : std_logic_vector (3 downto 0); -- Long enough for a 9 element vector
   signal input : std_logic_vector(7 downto 0);
-  signal output : std_logic_vector(7 downto 0);
-  signal address : std_logic_vector (2 downto 0) := "000"; -- 8 places
-  signal period: time := 10 ns;
+
+  signal tl : std_logic_vector(7 downto 0);
+  signal tc : std_logic_vector(7 downto 0);
+  signal tr : std_logic_vector(7 downto 0);
+  signal ml : std_logic_vector(7 downto 0);
+  signal mc : std_logic_vector(7 downto 0);
+  signal mr : std_logic_vector(7 downto 0);
+  signal bl : std_logic_vector(7 downto 0);
+  signal bc : std_logic_vector(7 downto 0);
+  signal br : std_logic_vector(7 downto 0);
+
 
 begin
   clock <= not clock after period/2 when finished='0';
 
-  BUFF: stencil_buffer port map (clock, advance, address, input, output);
+  BUFF: stencil_buffer port map (clock, advance, address, input,
+                                 tl, tc, tr,
+                                 ml, mc, mr,
+                                 bl, bc, br);
+
 
   process
   begin
     -- Fill test
+    advance <= '1';
     input <= "11001100";
-    advance <= '1';
-    wait for period;
+    wait for 9 * period;
 
-    advance <= '0';
-    address <= "000";
-    wait for period;
-    assert output = "11001100"
-      report "Ripple buffer should be content addressable" severity error;
+    assert tl = "11001100" report "tl should be filled" severity error;
+    assert tc = "11001100" report "tc should be filled" severity error;
+    assert tr = "11001100" report "tr should be filled" severity error;
+    assert ml = "11001100" report "ml should be filled" severity error;
+    assert mc = "11001100" report "mc should be filled" severity error;
+    assert mr = "11001100" report "mr should be filled" severity error;
+    assert bl = "11001100" report "bl should be filled" severity error;
+    assert bc = "11001100" report "bc should be filled" severity error;
+    assert br = "11001100" report "br should be filled" severity error;
 
-    address <= "001";
-    wait for period;
-    assert output = "00000000"
-      report "Only the first element should be written" severity error;
+    -- Order test
+    input <= "00000000"; wait for period;
+    input <= "00000001"; wait for period;
+    input <= "00000010"; wait for period;
+    input <= "00000011"; wait for period;
+    input <= "00000100"; wait for period;
+    input <= "00000101"; wait for period;
+    input <= "00000110"; wait for period;
+    input <= "00000111"; wait for period;
+    input <= "00001000"; wait for period;
 
-    advance <= '1';
-    wait for period;
+    assert tl = "00000000" report "tl should contain the correct value" severity error;
+    assert tc = "00000001" report "tc should contain the correct value" severity error;
+    assert tr = "00000010" report "tr should contain the correct value" severity error;
+    assert ml = "00000011" report "ml should contain the correct value" severity error;
+    assert mc = "00000100" report "mc should contain the correct value" severity error;
+    assert mr = "00000101" report "mr should contain the correct value" severity error;
+    assert bl = "00000110" report "bl should contain the correct value" severity error;
+    assert bc = "00000111" report "bc should contain the correct value" severity error;
+    assert br = "00001000" report "br should contain the correct value" severity error;
 
-    advance <= '0';
-    address <= "001";
-    wait for period;
-    assert output = "11001100"
-      report "The queue should now advance by one" severity error;
+    -- Scroll test
+    input <= "00001001"; wait for period;
+    input <= "00001010"; wait for period;
+    input <= "00001011"; wait for period;
 
-    wait for 10 * period;
+    assert tl = "00000011" report "tl should contain the correct value" severity error;
+    assert tc = "00000100" report "tc should contain the correct value" severity error;
+    assert tr = "00000101" report "tr should contain the correct value" severity error;
+    assert ml = "00000110" report "ml should contain the correct value" severity error;
+    assert mc = "00000111" report "mc should contain the correct value" severity error;
+    assert mr = "00001000" report "mr should contain the correct value" severity error;
+    assert bl = "00001001" report "bl should contain the correct value" severity error;
+    assert bc = "00001010" report "bc should contain the correct value" severity error;
+    assert br = "00001011" report "br should contain the correct value" severity error;
+
     finished <= '1';
     wait;
   end process;
